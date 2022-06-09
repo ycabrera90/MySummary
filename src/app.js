de8ff8dee5173610d7afa1a6a07f0b4387910d1c
yosniel <yosniel.ch@gmail.com>
2022-06-09 13:49:57 -0300

    Some bugs were fixed by showing the caroussel in small and large screens.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import { Map } from './UI/Map';
 import { validateEmail } from './Utility/inputsValidators';
 import { HttpReq } from './Utility/HTTP';
+import { Button } from 'bootstrap';
 
 const GITHUB_URL = 'https://github.com/ycabrera90';
 const PROTOCOL = 'https'
@@ -129,47 +130,88 @@ class ContacMeForm {
 
 class Caroussel {
     constructor() {
-        this.carousselIndicators = document.getElementById('carouselExampleIndicators');
-        this.showHideIndicators = this.carousselIndicators.querySelectorAll(".showHide-Indicator");
-        this.showHideCarousselItems = this.carousselIndicators.querySelectorAll('#caroussel-item-mySkills, #caroussel-item-myLocation');
+        this.caroussel = document.getElementById('carouselExampleIndicators');
 
-        window.addEventListener('resize',()=>{
-            console.log('esto cambio');
-            console.log(window.innerWidth);
-            console.log(window.innerHeight);
+        this.allIndicators = this.caroussel.querySelectorAll('.carousel-indicators button');
+        this.showHideIndicators = this.caroussel.querySelectorAll(".showHide-indicator");
+        this.mainIndicatior = this.caroussel.querySelector('.home-indicator');
+
+        this.allCarousselItems = this.caroussel.querySelectorAll('.carousel-item');
+        this.showHideCarousselItems = this.caroussel.querySelectorAll('#caroussel-item-mySkills, #caroussel-item-myLocation');
+        this.mainCarousselItem = this.caroussel.querySelector('#caroussel-item-home');
+
+
+        window.addEventListener('resize', () => {
+            if (window.innerWidth <= 768) {
+                this.renderSmall();
+            } else {
+                this.renderLarge();
+            }
         })
-
-
-
-
     }
 
-
-
     renderSmall() {
-        // show caroussel for small screens
-        for (const showHideIndicator of this.showHideIndicators) {
-            showHideIndicator.classList.rem('display-none');
-        }
-
+        var reset = false;
         for (const showHideCarousselItem of this.showHideCarousselItems) {
-            showHideCarousselItem.classList.rem('display-none');
-            showHideCarousselItem.classList.add('carousel-item');
+            if (!showHideCarousselItem.classList.contains('carousel-item')) {
+                showHideCarousselItem.classList.remove('display-none');
+                showHideCarousselItem.classList.add('carousel-item');
+                reset = true;
+            };
+        }
+        for (const showHideIndicator of this.showHideIndicators) {
+            if (showHideIndicator.classList.contains('display-none')) {
+                showHideIndicator.classList.remove('display-none');
+                reset = true;
+            };
+        }
+        if (reset) {
+            this.resetCaroussel();
         }
     }
 
     renderLarge() {
-        // show caroussel for large screens
-        for (const showHideIndicator of this.showHideIndicators) {
-            showHideIndicator.classList.add('display-none');
-        }
-
+        var reset = false;
         for (const showHideCarousselItem of this.showHideCarousselItems) {
-            showHideCarousselItem.classList.add('display-none');
-            showHideCarousselItem.classList.remove('carousel-item');
+            if (showHideCarousselItem.classList.contains('carousel-item')) {
+                showHideCarousselItem.classList.add('display-none');
+                showHideCarousselItem.classList.remove('carousel-item');
+                reset = true;
+            };
+        }
+        for (const showHideIndicator of this.showHideIndicators) {
+            if (!showHideIndicator.classList.contains('display-none')) {
+                showHideIndicator.classList.add('display-none');
+                reset = true;
+            };
+        }
+        if (reset) {
+            this.resetCaroussel();
         }
     }
+
+    resetCaroussel() {
+        // whe this method is excecuted the carouse go to the first slide
+        // clear of 'active' class all items
+        for (const carousselItem of this.allCarousselItems) {
+            if (carousselItem.classList.contains('active')) {
+                carousselItem.classList.remove('active');
+            }
+        }
+        // put class 'active' only the main item
+        this.mainCarousselItem.classList.add('active');
+
+        // clear of 'active' class all indicators
+        for (const indicator of this.allIndicators) {
+            if (indicator.classList.contains('active')) {
+                indicator.classList.remove('active');
+            }
+        }
+        // put class 'active' only the main indicator
+        this.mainIndicatior.classList.add('active');
+    }
 }
+
 
 
 
